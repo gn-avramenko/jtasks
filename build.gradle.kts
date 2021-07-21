5526fa9e5f674ffc652156ac1ce1e2d6b379924d
Vladimir Avramenko <vladimir.avramenko@gridnine.com>
2021-07-21 14:12:33 +0300

    init commit

---
+++ build.gradle.kts
@@ -0,0 +1,71 @@
+import com.gridnine.jasmine.gradle.plugin.jasmine
+
+buildscript {
+    dependencies{
+        "classpath"(files("submodules/jasmine/lib/spf-1.0.jar"))
+        "classpath"(files("gradle/jasmine-gradle-plugin.jar"))
+    }
+}
+plugins {
+    id("com.github.node-gradle.node") version("2.2.3")
+}
+
+
+apply<com.gridnine.jasmine.gradle.plugin.JasmineConfigPlugin>()
+
+
+jasmine {
+    kotlinVersion = "1.4.10"
+    kotlinCoroutinesJSVersion ="1.4.3"
+    libRelativePath = "submodules/jasmine/lib"
+    enableWebTasks = true
+    plugins("submodules/jasmine/plugins") {
+        plugin("com.gridnine.jasmine.common.core")
+        plugin("com.gridnine.jasmine.common.spf")
+        plugin("com.gridnine.jasmine.common.core.test")
+        plugin("com.gridnine.jasmine.server.core")
+        plugin("com.gridnine.jasmine.server.db.h2")
+        plugin("com.gridnine.jasmine.server.db.postgres")
+        plugin("com.gridnine.jasmine.server.core.test")
+        plugin("com.gridnine.jasmine.common.standard")
+        plugin("com.gridnine.jasmine.server.standard")
+        plugin("com.gridnine.jasmine.common.reports")
+        plugin("com.gridnine.jasmine.server.reports")
+        plugin("com.gridnine.jasmine.web.core")
+        plugin("com.gridnine.jasmine.web.core.test")
+        plugin("com.gridnine.jasmine.web.standard")
+        plugin("com.gridnine.jasmine.web.reports")
+        plugin("com.gridnine.jasmine.web.easyui")
+    }
+    plugins("plugins"){
+        plugin("com.gridnine.jtasks.common.core")
+        plugin("com.gridnine.jtasks.server.core")
+        plugin("com.gridnine.jtasks.server.core.test")
+        plugin("com.gridnine.jtasks.server.reports")
+        plugin("com.gridnine.jtasks.web.core")
+    }
+}
+
+repositories{
+    mavenCentral()
+}
+
+apply<com.gridnine.jasmine.gradle.plugin.JasminePlugin>()
+
+
+// task("deploy-locally", DeployApplicationTask::class){
+//     group = "jenkins"
+//     shouldRunAfter("jenkins-dist")
+//     host = "localhost"
+//     port = 21567
+// }
+
+
+//tasks.create("deploy-test", Deplo)
+
+//project.configurations.create("compile")
+//
+//dependencies{
+//    "compile"(files("submodules/jasmine/lib/spf-1.0.jar"))
+//}
+
